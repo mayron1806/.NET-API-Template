a134f96b31bb605a3cd457cb61fe5b8302187efe
mayron <mayronfernandes01@gmail.com>
2024-07-06 13:42:13 -0300

    add entities

---
+++ Domain/ActiveAccountToken.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain;
+
+public class ActiveAccountToken
+{
+    // Se necessário, você pode adicionar um construtor adicional para inicialização conveniente
+    public ActiveAccountToken(string content, int userId, DateTime expiresAt)
+    {
+        Content = content;
+        UserId = userId;
+        ExpiresAt = expiresAt;
+        CreatedAt = DateTime.UtcNow;
+    }
+    public int Id { get; }
+    public string Content { get; }
+    public User? User { get; }
+    public int UserId { get; }
+    public DateTime CreatedAt { get; }
+    public DateTime ExpiresAt { get; }
+}
---
+++ Domain/ResetPasswordToken.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain;
+
+public class ResetPasswordToken
+{
+    // Se necessário, você pode adicionar um construtor adicional para inicialização conveniente
+    public ResetPasswordToken(string content, int userId, DateTime expiresAt)
+    {
+        Content = content;
+        UserId = userId;
+        ExpiresAt = expiresAt;
+        CreatedAt = DateTime.UtcNow;
+    }
+    public int Id { get; }
+    public string Content { get; }
+    public User? User { get; }
+    public int UserId { get; }
+    public DateTime CreatedAt { get; }
+    public DateTime ExpiresAt { get; }
+}
