bed84a7729680b0daaf70c0a4d7cccc9adc02b81
mayron <mayronfernandes01@gmail.com>
2024-07-07 08:02:24 -0300

    add services

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,16 +1,31 @@
-﻿using Infrastructure.UnitOfWork;
+﻿using Infrastructure.Services.Email;
+using Infrastructure.Services.JWT;
+using Infrastructure.Settings;
+using Infrastructure.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
+        // db
         services.AddDbContext<DatabaseContext>(options => options.UseSqlite("Data Source=../database.db"));
         services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
 
+        // settings
+        var smtp = configuration.GetSection("SMTP").Get<SMTPSettings>() ?? throw new Exception("SMTP settings not found");
+        var jwt = configuration.GetSection("JWT").Get<JWTSettings>() ?? throw new Exception("JWT settings not found");
+        services.AddSingleton(smtp);
+        services.AddSingleton(jwt);
+
+        // services
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<JWTService>();
+        
         return services;
     }
 }
---
+++ Infrastructure/Services/Email/IEmailService.cs
@@ -0,0 +1,7 @@
+namespace Infrastructure.Services.Email
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(string to, string subject, string body);
+    }
+}
